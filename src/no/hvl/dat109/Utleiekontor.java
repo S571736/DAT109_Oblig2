777e1b8d790c392f35072c89e8c182a6dc4f3f78
Sander <571368@stud.hvl.no>
2021-02-05 11:21:36 +0100

    get/set bil, addresse og kunde

--- src/no/hvl/dat109/Utleiekontor.java
+++ src/no/hvl/dat109/Utleiekontor.java
@@ -1,17 +1,60 @@
 package no.hvl.dat109;
 
+import java.util.List;
+
 public class Utleiekontor {
 
+    private String kontorNavn;
     private int kontorNummer;
-    private int kontorAdresse;
+    private Adresse kontorAdresse;
     private Reservasjon[] reservasjoner;
+    private List<Bil> biler;
 
 
-    public Utleiekontor(int kontorNummer, int kontorAdresse, Reservasjon[] reservasjoner){
 
-        this.kontorNummer = kontorNummer;
+    public Utleiekontor(String kontorNavn, Adresse kontorAdresse, Reservasjon[] reservasjoner){
+
+        this.kontorNavn = kontorNavn;
+        this.kontorNummer = kontorNummer++;
         this.kontorAdresse = kontorAdresse;
         this.reservasjoner = reservasjoner;
 
     }
+
+    public String getNavn() {
+        return kontorNavn;
+    }
+
+    public void setNavn(String kontorNavn) {
+        this.kontorNavn = kontorNavn;
+    }
+
+    public int getKontorNummer() {
+        return kontorNummer;
+    }
+
+    public Adresse getAdresse() {
+        return kontorAdresse;
+    }
+
+    public void setAdresse(Adresse adresse) {
+        this.kontorAdresse = adresse;
+    }
+
+
+    public void leggTilBil(Bil bil) {
+       // TODO
+    }
+
+    public void setBiler(List<Bil> biler) {
+        // TODO
+    }
+
+    public List<Bil> getBiler() {
+        // TODO
+        return biler;
+    }
+
+
+
 }
