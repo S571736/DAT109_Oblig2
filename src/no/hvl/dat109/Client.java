821f60f181a11326d493d63a198be7f8ca5d43b9
Sondre <Sondre.Gjesdal@gmail.com>
2021-02-11 16:10:41 +0100

    some more work on save function

--- src/no/hvl/dat109/Client.java
+++ src/no/hvl/dat109/Client.java
@@ -2,6 +2,9 @@ package no.hvl.dat109;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,17 +14,47 @@ public class Client {
     /**
      * Just some testvalues, maybe deletable after the save/load methods are working somehow
      */
-    Bil[] havispark = new Bil[20];
-    Utleiekontor[] havisKontor = new Utleiekontor[5];
-    Adresse adr1 = new Adresse("Strengveien", 8888, "Streng");
-    Bilutleie havis = new Bilutleie("Havis", 888888, adr1, havispark, havisKontor);
+    private Bil[] havispark = new Bil[20];
+    private Utleiekontor[] havisKontor = new Utleiekontor[5];
+    private Adresse adr1 = new Adresse("Strengveien", 8888, "Streng");
+    private Kunde kunde1 = new Kunde("Ola", "Nordmann", 999, adr1);
+    private Bilutleie havis = new Bilutleie("Havis", 888888, adr1, havispark, havisKontor);
+    private Kunde[] kunder = {kunde1};
+    private Adresse[] adresser = {adr1};
+
 
     //TODO: Need to find somehow to save the Lists and objects within objects easier without much work, not part of main task
+
+
     public void start() {
         System.out.println("-------Main menu-------");
         System.out.println("1. Load file");
         System.out.println("2. Print something");
         System.out.println("3. Fuckings reservere ein bil eller noe?");
+        System.out.println("4. Save data");
+
+        Scanner scan = new Scanner(System.in);
+        int menu = Integer.parseInt(scan.nextLine());
+        switch (menu){
+            case 1:
+                readFile();
+                start();
+                break;
+            case 2:
+                System.out.println("Wow, you printed something!");
+                start();
+                break;
+            case 3:
+                System.out.println("Function not implemented");
+                start();
+                break;
+            case 4:
+                writeFile(havis, kunder, havisKontor[0], havispark, adresser);
+                break;
+            default:
+                break;
+
+        }
     }
 
     /**
@@ -62,6 +95,34 @@ public class Client {
 
     }
 
+
+    /**
+     * For testing lagrer vi bare et bilutleie, med et enkelt kontor
+     */
+    private void writeFile(Bilutleie utleie, Kunde[] kunder, Utleiekontor kontor, Bil[] biler, Adresse[] adresser) {
+        File file = new File("save.txt");
+        try {
+            FileWriter writer = new FileWriter("save.txt");
+            writer.write(utleie.toString());
+            writer.write(kontor.toString());
+
+            for (Kunde k : kunder) {
+                writer.write(k.toString());
+            }
+            for (Bil b: biler) {
+                writer.write(b.toString());
+            }
+            for (Adresse a:adresser) {
+                writer.write(a.toString());
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
     private Adresse makeAdresse(List<String> object) {
         Adresse adr;
         adr = new Adresse(object.get(0), Integer.parseInt(object.get(1)), object.get(2));
@@ -96,8 +157,6 @@ public class Client {
                 Integer.parseInt(object.get(5)));
         return bil;
     }
-
-
 
 
 }
